993455fc2d06c9de0cdcb77b1dfb7191e3f60290
Wlad <wlobanow@mail.ru>
2022-05-11 17:16:24 +0300

    поучился 11.05.22

---
+++ For tests/js/script2.js
@@ -0,0 +1,61 @@
+'use strict';
+
+//////////////////////////////////////////////////////
+
+// const shoppingMallData = {
+//     shops: [{
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// }
+
+// function isBudgetEnough(data) {
+//     let square = 0;
+//     let val = 0;
+//     let cost = 0;
+//     for (let key in data.shops) {
+//         for (let bey of data.shops) {
+//             square += bey.width * bey.length;
+//         }
+//         console.log(square);
+//         break;
+//     }
+
+//     val = square * data.height;
+//     console.log(val);
+//     cost = val * data.moneyPer1m3;
+//     console.log(cost);
+
+//     if (cost < data.budget) {
+//         return 'Бюджета достаточно';
+//     } else {
+//         return 'Бюджета недостаточно';
+//     }
+
+// }
+
+// console.log(isBudgetEnough(shoppingMallData));
+
+////////////////////////////////////////////////////////////
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+
+}
