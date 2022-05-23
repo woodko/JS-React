0902f6157c4616c537d858bc3628f51a63c80104
Wlad <wlobanow@mail.ru>
2022-05-23 10:14:48 +0300

    Поучился 20.05.22

---
+++ Задачи с собеседований на понимание основ/js/script.js
@@ -0,0 +1,34 @@
+// 'use strict';
+
+// // let x = 5; alert(x++);
+
+// // [] + false - null + true;
+// // console.log(typeof([] + false));  //false
+// // console.log([] + false - null + true); // NaN
+
+// // let y = 1;
+// // let x = y = 2;
+// // alert(x);
+
+// // console.log([] + 1 + 2); //12
+
+// // alert("1" [0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+// И запинается на ЛЖИ
+// ИЛИ запинается на ПРАВДЕ
+
+// console.log(!!(1 && 2) === (1 && 2));
+//    3
+// alert(null || 2 && 3 || 4);
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+
+// console.log(a == b); //false
+
+// alert( +"Infinity" );  infinity - number
+
+// console.log("Ёжик" > "яблоко"); // false
+
+// console.log(0 || "" || 2 || undefined || true || falsе);
